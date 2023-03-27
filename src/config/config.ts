0b2698819e1d97d7f5b9370544010ad768774fa8
Fernando Cardoso <101157811+fernandoreki@users.noreply.github.com>
2023-03-27 09:21:13 -0600

    Add files via upload

---
+++ src/config/config.ts
@@ -0,0 +1,35 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const MONGO_USERNAME = process.env.MONGO_USERNAME;
+const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
+const MONGO_HOST = process.env.MONGO_HOST;
+
+const MONGO = {
+  host: MONGO_HOST,
+  username: MONGO_USERNAME,
+  password: MONGO_PASSWORD
+}
+
+const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || "localhost";
+const SERVER_PORT = process.env.SERVER_PORT || "3001";
+
+const SERVER = {
+  hostname: SERVER_HOSTNAME,
+  port: SERVER_PORT,
+};
+
+const TOKEN_SECRET = process.env.TOKEN_SECRET || "";
+
+const SECRETS = {
+  token: TOKEN_SECRET,
+};
+
+const config = {
+  server: SERVER,
+  secrets: SECRETS,
+  mongo: MONGO
+};
+
+export default config;
